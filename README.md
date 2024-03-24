a2d792fac633d782f551ca66cd30051e481215d4
DeveloperNilesh <nileshmukherjee2017@gmail.com>
2024-03-24 23:46:08 +0530

    Weather App

--- README.md
+++
@@ -1,1 +0,0 @@
-Through this app people can see daily news of any topic they are searching for.
--- script.js
+++ script.js
@@ -1,158 +1,157 @@
-const URL = "https://newsapi.org/v2/everything?q=";
-const API_KEY = "48dc65708fb844fa962374ae9991338e";
-
-const newsInput = document.querySelector("#search-text")
-const cardsContainer = document.querySelector("#cards-container");
-const newsCardTemplate = document.querySelector("#template-news-card");
-const searchBtn = document.querySelector("#search-button");
-const loaderContainer = document.querySelector(".loader-container");
-const icon = document.querySelector("#set-theme");
-const hamburgerIcon = document.querySelector(".hamburger-icon")
-const hamCrossIcon = document.querySelector(".ham-cross-icon")
+const userTab = document.querySelector("[data-userWeather]");
+const searchTab = document.querySelector("[data-searchWeather]");
+// const userContainer = document.querySelector(".weather-container");
+const grantAccessContainer = document.querySelector(".grant-location-container");
+const searchForm = document.querySelector("[data-searchForm]");
+const loadingScreen = document.querySelector(".loading-container");
+const userInfoContainer = document.querySelector(".user-info-container");
 
 
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 
-// Pick yesterday's date
-let yesterday = new Date();
-yesterday.setDate(yesterday.getDate() - 1);
+let oldTab = userTab;
+oldTab.classList.add("current-tab");
+getfromSessionStorage();
 
-// get date in yyyy-mm-dd format
-function getFormattedDate(dt){
-    let dd = dt.getDate();
-    let mm = dt.getMonth() + 1;
-    let yyyy = dt.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd;
-    }
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-    return (yyyy + '-' + mm + '-' + dd);
-}
-
-// Get yesterday's date
-let yesterDate = getFormattedDate(yesterday);
-console.log(yesterDate);
-
-
-
-window.addEventListener('load', ()=> fetchNews("India"))
-
-function reloadNews(){
-    window.location.reload()
-}
-
-async function fetchNews(query){
-    cardsContainer.innerHTML = "";
-    loaderContainer.classList.remove("hide");
-    let url = `${URL}${query}&from=${yesterDate}&to=${yesterDate}&sortBy=popularity&apiKey=${API_KEY}`
-    console.log(url)
-    const response = await fetch(url)
-    const data = await response.json();
-    if(data){
-        loaderContainer.classList.add("hide")
-    }
-
-    getData(data.articles)
-}
-
-function getData(articles){
-    articles.forEach(article => {
-        if(!article.urlToImage || !article.description || (!article.urlToImage && !article.description)){
-            return;
+function switchTab(newTab) {
+    if(newTab != oldTab){
+        oldTab.classList.remove("current-tab");
+        oldTab = newTab;
+        oldTab.classList.add("current-tab");
+        if(!searchForm.classList.contains("active")) {
+            //kya search form wala container is invisible, if yes then make it visible
+            userInfoContainer.classList.remove("active");
+            grantAccessContainer.classList.remove("active");
+            searchForm.classList.add("active");
+        } else {
+            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
+            searchForm.classList.remove("active");
+            userInfoContainer.classList.remove("active");
+            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
+            //for coordinates, if we haved saved them there.
+            getfromSessionStorage();
         }
-        else {
-            try {
-                let content = newsCardTemplate.content.cloneNode(true);
-
-                let image = content.querySelector('#news-img');
-                image.src = `${article.urlToImage}`
-
-                let newsTitle = content.querySelector("#news-title")
-                newsTitle.innerText = `${article.title}`
-
-                let date = new Date(article.publishedAt).toLocaleString("en-US",{
-                    timeZone: "Asia/Jakarta",
-                })
-                let newsSrc = content.querySelector("#news-source")
-                newsSrc.innerHTML = `${article.source.name} - ${date}`
-
-                let newsDesc = content.querySelector("#news-desc")
-                let descNews = article.description
-                
-                if(descNews.length < 120){
-                    newsDesc.innerText = `${descNews}`
-                }
-                else{
-                    newsDesc.innerText = `${descNews.substring(0,120)}...`
-                }
-
-                content.firstElementChild.addEventListener("click", () => {
-                    window.open(article.url, "_blank")
-                })
-
-                cardsContainer.appendChild(content);
-            } 
-            catch (error) {
-                throw error;
-            }
-        }
-    });
+    }
 }
 
-let currentSelectedNav = null
-function onNavItemClick(topic){
-    fetchNews(topic)
-    const navItem = document.getElementById(topic)
-    if(currentSelectedNav !== null){
-        currentSelectedNav.classList.remove("active")
+userTab.addEventListener("click", () => {
+    //pass clicked tab as input paramter
+    switchTab(userTab);
+});
+
+searchTab.addEventListener("click", () => {
+    //pass clicked tab as input paramter
+    switchTab(searchTab);
+});
+
+//check if cordinates are already present in session storage
+function getfromSessionStorage() {
+    const localCoordinates = sessionStorage.getItem("user-coordinates");
+    if(!localCoordinates) {
+        //agar local coordinates nahi mile
+        grantAccessContainer.classList.add("active");
     }
-    currentSelectedNav = navItem;
-    currentSelectedNav.classList.add("active")
+    else {
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
 }
 
+async function fetchUserWeatherInfo(coordinates) {
+    const {lat, lon} = coordinates;
+    // make grantcontainer invisible
+    grantAccessContainer.classList.remove("active");
+    //make loader visible
+    loadingScreen.classList.add("active");
 
-searchBtn.addEventListener("click", () => {
-    searchNews();
-})
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        const  data = await response.json();
 
-newsInput.addEventListener("keydown", (event)=>{
-    if (event.key === "Enter") {
-        searchNews();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
     }
-})
-
-function searchNews(){
-    let searchFor = newsInput.value;
-    if(!searchFor) return;
-    fetchNews(`${searchFor}`)
-    if(currentSelectedNav !== null){
-        currentSelectedNav.classList.remove("active")
+    catch(err) {
+        loadingScreen.classList.remove("active");
+        //HW
     }
-    currentSelectedNav = null;
 }
 
-icon.addEventListener("click", () => {
-    icon.style.transform = "scale(0.8)";
-    // After a short delay, revert the scale to 1
-    setTimeout(() => {
-        icon.style.transform = "scale(1)";
-    }, 100);
-    
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        icon.src = "./sun.png";
+function renderWeatherInfo(weatherInfo) {
+    //fistly, we have to fethc the elements 
+
+    const cityName = document.querySelector("[data-cityName]");
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const desc = document.querySelector("[data-weatherDesc]");
+    const weatherIcon = document.querySelector("[data-weatherIcon]");
+    const temp = document.querySelector("[data-temp]");
+    const windspeed = document.querySelector("[data-windspeed]");
+    const humidity = document.querySelector("[data-humidity]");
+    const cloudiness = document.querySelector("[data-cloudiness]");
+
+    //fetch values from weatherINfo object and put it UI elements
+    cityName.innerText = weatherInfo?.name;
+    countryIcon.src = `https://flagcdn.com/144x108/${weatherInfo?.sys?.country.toLowerCase()}.png`;
+    desc.innerText = weatherInfo?.weather?.[0]?.description;
+    weatherIcon.src = `http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
+    temp.innerText = `${weatherInfo?.main?.temp} °C`;
+    windspeed.innerText = `${weatherInfo?.wind?.speed} m/s`;
+    humidity.innerText = `${weatherInfo?.main?.humidity}%`;
+    cloudiness.innerText = `${weatherInfo?.clouds?.all}%`; 
+}
+
+function getLocation() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
     }
-    else{
-        icon.src = "./moon.png";
+    else {
+        alert("Geolocation is not supported by this browser.");
     }
+}
+
+function showPosition(position) {
+
+    const userCoordinates = {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude,
+    }
+
+    sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
+    fetchUserWeatherInfo(userCoordinates);
+
+}
+
+const grantAccessButton = document.querySelector("[data-grantAccess]");
+grantAccessButton.addEventListener("click", getLocation);
+
+const searchInput = document.querySelector("[data-searchInput]");
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let cityName = searchInput.value;
+
+    if(cityName === "")
+        return;
+    else 
+        fetchSearchWeatherInfo(cityName);
 })
 
-hamburgerIcon.addEventListener("click",() => {
-    document.querySelector(".hamburger").style.right = "0px"
-    document.querySelector(".hamburger").style.display = "block"
-})
-hamCrossIcon.addEventListener("click",() => {
-    document.querySelector(".hamburger").style.right = "-160px"
-    // document.querySelector(".hamburger").style.display = "block"
-})
+async function fetchSearchWeatherInfo(city) {
+    loadingScreen.classList.add("active");
+    userInfoContainer.classList.remove("active");
+    grantAccessContainer.classList.remove("active");
+
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+          );
+        const data = await response.json();
+        loadingScreen.classList.remove("active");
+        userInfoContainer.classList.add("active");
+        renderWeatherInfo(data);
+    }
+    catch(err) {
+        //hW
+    }
+}
